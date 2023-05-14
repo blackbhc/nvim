ce39c5e42788c0856006cb61f51d3075bd17b3c3
blackbhc <blackbhc@icloud.com>
2023-05-14 23:33:06 +0800

    add: my self convention of key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- <leader> key
+vim.g.mapleader = " "
+
+-- other keymaps
+vim.keymap.set("i", "<C-z>", "<Esc><C-z>") -- C-z in insert mode
+vim.keymap.set({ "n", "i" }, "<C-a>", "<Esc>I") -- C-a for insert at the begin
+vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end
+vim.keymap.set({ "o", "i" }, "<C-o>", "<Esc>o") -- C-o for insert a new line in insert/normal mode
+vim.keymap.set({ "i" }, "<C-h>", "<cmd>noh<cr>i") -- C-h to toggle hight in insert mode
+vim.keymap.set({ "n" }, "<C-h>", "<cmd>noh<cr>") -- C-h for toggle hight in normal mode
