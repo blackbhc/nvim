448f2603799a7b4f0b8318ab21958d8dec0fda87
blackbhc <blackbhc@icloud.com>
2023-05-17 11:23:59 +0800

    dev: C-x and C-c for fast move into the begin and end of the file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,10 @@
 
 -- other keymaps
 vim.keymap.set("i", "<C-z>", "<Esc><C-z>") -- C-z in insert mode
-vim.keymap.set({ "n", "i" }, "<C-a>", "<Esc>I") -- C-a for insert at the begin
-vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end
+vim.keymap.set({ "n", "i" }, "<C-a>", "<Esc>I") -- C-a for insert at the begin of line
+vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end of line
+vim.keymap.set({ "n", "i" }, "<C-x>", "<Esc>ggI") -- C-x for insert at the begin of the file
+vim.keymap.set({ "n", "i" }, "<C-c>", "<Esc>GA") -- C-c for insert at the end of the file
 vim.keymap.set({ "o", "i" }, "<C-o>", "<Esc>o") -- C-o for insert a new line in insert/normal mode
 vim.keymap.set({ "i" }, "<C-h>", "<cmd>noh<cr>i") -- C-h to toggle hight in insert mode
 vim.keymap.set({ "n" }, "<C-h>", "<cmd>noh<cr>") -- C-h for toggle hight in normal mode
