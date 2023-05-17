5ac903724d8dee57b612abcd60c9193d25fa3dbb
blackbhc <blackbhc@icloud.com>
2023-05-17 10:47:38 +0800

    C-q: for fast quit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,5 @@ vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end
 vim.keymap.set({ "o", "i" }, "<C-o>", "<Esc>o") -- C-o for insert a new line in insert/normal mode
 vim.keymap.set({ "i" }, "<C-h>", "<cmd>noh<cr>i") -- C-h to toggle hight in insert mode
 vim.keymap.set({ "n" }, "<C-h>", "<cmd>noh<cr>") -- C-h for toggle hight in normal mode
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>") -- C-s for save in normal/insert mode
+vim.keymap.set({ "n", "i" }, "<C-q>", "<cmd>q<cr>") -- C-q for exit in normal/insert mode
