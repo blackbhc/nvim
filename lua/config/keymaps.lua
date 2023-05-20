0e2b79cff52fd53d94913a87b4f966177e2afaf5
blackbhc <blackbhc@icloud.com>
2023-05-20 10:24:30 +0800

    dev: C-c for fast esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,10 +6,7 @@
 vim.keymap.set("i", "<C-z>", "<Esc><C-z>") -- C-z in insert mode
 vim.keymap.set({ "n", "i" }, "<C-a>", "<Esc>I") -- C-a for insert at the begin of line
 vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end of line
-vim.keymap.set({ "n", "i" }, "<C-x>", "<Esc>ggI") -- C-x for insert at the begin of the file
-vim.keymap.set({ "n", "i" }, "<C-c>", "<Esc>GA") -- C-c for insert at the end of the file
-vim.keymap.set({ "o", "i" }, "<C-o>", "<Esc>o") -- C-o for insert a new line in insert/normal mode
+vim.keymap.set({ "n", "i" }, "<C-c>", "<Esc>GA") -- C-c for fast <Esc>
 vim.keymap.set({ "i" }, "<C-h>", "<cmd>noh<cr>i") -- C-h to toggle hight in insert mode
 vim.keymap.set({ "n" }, "<C-h>", "<cmd>noh<cr>") -- C-h for toggle hight in normal mode
-vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr>") -- C-s for save in normal/insert mode
 vim.keymap.set({ "n", "i" }, "<C-q>", "<cmd>q<cr>") -- C-q for exit in normal/insert mode
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,9 +6,9 @@ return {
       add = { text = " " },
       change = { text = " " },
       delete = { text = " " },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-      untracked = { text = " " },
+      topdelete = { text = "󰼉 " },
+      changedelete = { text = "󰃴 " },
+      untracked = { text = "󰗞 " },
     },
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
