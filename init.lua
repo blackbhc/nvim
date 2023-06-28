f8ca31a8ae1206adf11090495d3bb00bb79473ee
blackbhc <blackbhc@icloud.com>
2023-06-28 21:08:57 +0800

    dev mod: enable copilot for markdown

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.o.termguicolors = true
+vim.g.copilot_filetypes = { markdown = true }
+-- enable copilot for markdown files
 vim.cmd("source $HOME/.config/nvim/vimrc.vim")
