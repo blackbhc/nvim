07524548b6983052b816ee15d65e4afa06f65efc
blackbhc <blackbhc@icloud.com>
2023-06-28 23:10:20 +0800

    dev add: plugin/lsp..., enable the LSP for C(++), python, ...

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,283 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+    { "folke/neodev.nvim", opts = {} },
+    "mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      cond = function()
+        return require("lazyvim.util").has("nvim-cmp")
+      end,
+    },
+  },
+  ---@class PluginLspOpts
+  opts = {
+    -- options for vim.diagnostic.config()
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
+        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        -- prefix = "icons",
+      },
+      severity_sort = true,
+    },
+    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the inlay hints.
+    inlay_hints = {
+      enabled = false,
+    },
+    -- add any global capabilities here
+    capabilities = {},
+    -- Automatically format on save
+    autoformat = true,
+    -- Enable this to show formatters used in a notification
+    -- Useful for debugging formatter issues
+    format_notify = false,
+    -- options for vim.lsp.buf.format
+    -- `bufnr` and `filter` is handled by the LazyVim formatter,
+    -- but can be also overridden when specified
+    format = {
+      formatting_options = nil,
+      timeout_ms = nil,
+    },
+    -- LSP Server Settings
+    ---@type lspconfig.options
+    servers = {
+      jsonls = {},
+      lua_ls = {
+        -- mason = false, -- set to false if you don't want this server to be installed with mason
+        settings = {
+          Lua = {
+            workspace = {
+              checkThirdParty = false,
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      },
+      -- c++ language server, with clangd as the default and with many features
+      clangd = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--completion-style=bundled",
+          "--header-insertion=iwyu",
+          "--suggest-missing-includes",
+          "--cross-file-rename",
+          "--clang-tidy-checks=-*,bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,google-*,llvm-*,misc-*,modernize-*,performance-*,portability-*,readability-*",
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_dir = require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+        init_options = {
+          clangdFileStatus = true,
+          usePlaceholders = true,
+          completeUnimported = true,
+          semanticHighlighting = true,
+        },
+        settings = {
+          ccls = {
+            index = {
+              threads = 0,
+            },
+            clang = {
+              excludeArgs = { "-frounding-math" },
+            },
+          },
+        },
+      },
+      -- python language server, with many features open
+      pyright = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_dir = require("lspconfig.util").root_pattern(
+          ".git",
+          "setup.py",
+          "setup.cfg",
+          "pyproject.toml",
+          "requirements.txt"
+        ),
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+              typeCheckingMode = "basic",
+              stubPath = vim.fn.stdpath("data") .. "/stubs",
+            },
+          },
+        },
+      },
+      -- fortran language server, with many features open
+      fortls = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "fortls" },
+        filetypes = { "fortran" },
+        root_dir = require("lspconfig.util").root_pattern(".git", "Makefile", "meson.build", "CMakeLists.txt"),
+      },
+      -- rust language server, with many features open
+      rust_analyzer = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "rust-analyzer" },
+        filetypes = { "rust" },
+        root_dir = require("lspconfig.util").root_pattern("Cargo.toml", "rust-project.json", ".git"),
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              loadOutDirsFromCheck = true,
+            },
+            procMacro = {
+              enable = true,
+            },
+          },
+        },
+      },
+      -- bash language server, with many features open
+      bashls = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "bash-language-server", "start" },
+        filetypes = { "sh", "zsh", "bash" },
+        root_dir = require("lspconfig.util").root_pattern(".git", "Makefile", "meson.build", "CMakeLists.txt"),
+      },
+      -- cmake language server, with many features open
+      cmake = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "cmake-language-server" },
+        filetypes = { "cmake" },
+        root_dir = require("lspconfig.util").root_pattern(".git", "Makefile", "meson.build", "CMakeLists.txt"),
+      },
+      -- java language server, with many features open
+      jdtls = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "jdtls" },
+        filetypes = { "java" },
+        root_dir = require("lspconfig.util").root_pattern(".git", "Makefile", "meson.build", "CMakeLists.txt"),
+      },
+      -- html language server, with many features open
+      html = {
+        -- mason = true, -- set to false if you don't want this server to be installed with mason
+        cmd = { "html-languageserver", "--stdio" },
+        filetypes = { "html" },
+        root_dir = require("lspconfig.util").root_pattern(".git", "Makefile", "meson.build", "CMakeLists.txt"),
+      },
+    },
+    -- you can do any additional lsp server setup here
+    -- return true if you don't want this server to be setup with lspconfig
+    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
+    },
+  },
+  ---@param opts PluginLspOpts
+  config = function(_, opts)
+    local Util = require("lazyvim.util")
+    -- setup autoformat
+    require("lazyvim.plugins.lsp.format").setup(opts)
+    -- setup formatting and keymaps
+    Util.on_attach(function(client, buffer)
+      require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+    end)
+
+    -- diagnostics
+    for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
+      name = "DiagnosticSign" .. name
+      vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+    end
+
+    if opts.inlay_hints.enabled and vim.lsp.buf.inlay_hint then
+      Util.on_attach(function(client, buffer)
+        if client.server_capabilities.inlayHintProvider then
+          vim.lsp.buf.inlay_hint(buffer, true)
+        end
+      end)
+    end
+
+    if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
+      opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
+        or function(diagnostic)
+          local icons = require("lazyvim.config").icons.diagnostics
+          for d, icon in pairs(icons) do
+            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+              return icon
+            end
+          end
+        end
+    end
+
+    vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+    local servers = opts.servers
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      require("cmp_nvim_lsp").default_capabilities(),
+      opts.capabilities or {}
+    )
+
+    local function setup(server)
+      local server_opts = vim.tbl_deep_extend("force", {
+        capabilities = vim.deepcopy(capabilities),
+      }, servers[server] or {})
+
+      if opts.setup[server] then
+        if opts.setup[server](server, server_opts) then
+          return
+        end
+      elseif opts.setup["*"] then
+        if opts.setup["*"](server, server_opts) then
+          return
+        end
+      end
+      require("lspconfig")[server].setup(server_opts)
+    end
+
+    -- get all the servers that are available thourgh mason-lspconfig
+    local have_mason, mlsp = pcall(require, "mason-lspconfig")
+    local all_mslp_servers = {}
+    if have_mason then
+      all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+    end
+
+    local ensure_installed = { clangd, pyright } ---@type string[]
+    for server, server_opts in pairs(servers) do
+      if server_opts then
+        server_opts = server_opts == true and {} or server_opts
+        -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+        if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
+          setup(server)
+        else
+          ensure_installed[#ensure_installed + 1] = server
+        end
+      end
+    end
+
+    if have_mason then
+      mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
+    end
+
+    if Util.lsp_get_config("denols") and Util.lsp_get_config("tsserver") then
+      local is_deno = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+      Util.lsp_disable("tsserver", is_deno)
+      Util.lsp_disable("denols", function(root_dir)
+        return not is_deno(root_dir)
+      end)
+    end
+  end,
+}
