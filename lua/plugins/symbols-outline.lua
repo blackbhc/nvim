5ae537c3fcc49b9894c48a42a2cc9b63e5ffb83b
blackbhc <blackbhc@icloud.com>
2023-10-12 19:56:19 +0800

    add symbols-outline.nvim

---
+++ lua/plugins/symbols-outline.lua
@@ -0,0 +1,22 @@
+return {
+  "simrat39/symbols-outline.nvim",
+  keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+  cmd = "SymbolsOutline",
+  opts = function()
+    local Config = require("lazyvim.config")
+    local defaults = require("symbols-outline.config").defaults
+    local opts = {
+      symbols = {},
+      symbol_blacklist = {},
+    }
+
+    for kind, symbol in pairs(defaults.symbols) do
+      opts.symbols[kind] = {
+        icon = Config.icons.kinds[kind] or symbol.icon,
+        hl = symbol.hl,
+      }
+      table.insert(opts.symbol_blacklist, kind)
+    end
+    return opts
+  end,
+}
