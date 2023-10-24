b582344a6c5d54eadabb572e7c2b1a966ba44665
blackbhc <blackbhc@icloud.com>
2023-10-25 02:15:58 +0800

    use // for C comment

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -2,6 +2,16 @@ return {
   "numToStr/Comment.nvim",
   config = function()
     require("Comment").setup()
+    local ft = require("Comment.ft")
+    -- 1. Using set function
+    ft
+      -- Set only line comment
+      .set("yaml", "#%s")
+      -- Or set both line and block commentstring
+      .set("javascript", { "//%s", "/*%s*/" })
+    -- 2. Metatable magic
+    ft.cpp = { "//%s", "/*\n%s\n*/" }
+    ft.ini = "#%s"
   end,
   event = "VeryLazy",
 }
