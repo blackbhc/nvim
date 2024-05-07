8740c27b5230481ef57a3c00dfa3c3b4430dd299
blackbhc <blackbhc@icloud.com>
2024-05-07 18:15:44 +0800

    colorscheme: tokyonight-moon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
-  "tokyonight.nvim",
+  "folke/tokyonight.nvim",
   lazy = true,
   priority = 1000,
   opts = function()
     return {
       transparent = true,
+      style = "moon",
     }
   end,
 }
