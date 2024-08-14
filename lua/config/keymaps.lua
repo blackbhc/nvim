cf62308438826df94a8cf7fe283e3288e058345f
blackbhc <blackbhc@icloud.com>
2024-08-14 12:04:27 +0800

    remove some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,14 +6,3 @@
 vim.keymap.set("i", "<C-z>", "<Esc><C-z>") -- C-z in insert mode
 vim.keymap.set({ "n", "i" }, "<C-a>", "<Esc>I") -- C-a for insert at the begin of line
 vim.keymap.set({ "n", "i" }, "<C-e>", "<Esc>A") -- C-e for insert at the end of line
-vim.keymap.set({ "n", "i" }, "<C-c>", "<Esc>") -- C-c for fast <Esc>
-vim.keymap.set({ "n", "i" }, "<C-q>", "<cmd>q<cr>") -- C-q for exit in normal/insert mode
-vim.keymap.set({ "n", "i" }, "<C-x>", "<cmd>wq<cr>") -- C-x for write and exit in normal/insert mode
--- similar shortcuts as in emacs
-vim.keymap.set({ "i", "n" }, "<A-b>", "<Esc>bi") -- A-b for move to the previous word
-vim.keymap.set({ "i", "n" }, "<A-f>", "<Esc>ea") -- A-f for move to the next word
-vim.keymap.set({ "i", "n" }, "<A-d>", "<Esc>ldwi") -- A-b for move to the previous word
-vim.keymap.set({ "i", "n" }, "<A-BS>", "<Esc>bdwi") -- A-f for move to the next word
--- whole file movements
-vim.keymap.set({ "i", "n" }, "<A-a>", "<Esc>ggI") -- A-a for move to the beginning of file
-vim.keymap.set({ "i", "n" }, "<A-e>", "<Esc>GA") -- A-e for move to the end of file
